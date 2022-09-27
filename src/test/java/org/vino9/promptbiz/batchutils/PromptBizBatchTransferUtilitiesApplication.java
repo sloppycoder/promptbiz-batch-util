5c9cf8178d65b37681b846ec802b595ff747017c
Li Lin <guru.lin@gmail.com>
2022-09-27 19:22:10 +0800

    initial skelelton

---
+++ src/test/java/org/vino9/promptbiz/batchutils/PromptBizBatchTransferUtilitiesApplication.java
@@ -0,0 +1,13 @@
+package org.vino9.promptbiz.batchutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptBizBatchTransferUtilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptBizBatchTransferUtilitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vino9/promptbiz/batchutils/PromptBizBatchTransferUtilitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vino9.promptbiz.batchutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptBizBatchTransferUtilitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
